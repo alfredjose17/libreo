e0399190a5ee69ec5b3e2c6868b12369d29194e7
overlord839 <cristomathew7@gmail.com>
2020-02-09 21:01:04 +0530

    latest@working

--- src/app/pages/maps/maps.page.ts
+++ src/app/pages/maps/maps.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation'; 
-import { NavController } from 'ionic-angular';
+import {  NavController } from '@ionic/angular';
 declare var google;
 @Component({
   selector: 'app-maps',
@@ -29,57 +29,58 @@ export class MapsPage {
   ionViewDidEnter(){
     this.getUserPosition();
   }   
-  getRestaurants(latLng)
-  {
-      var service = new google.maps.places.PlacesService(this.map);
-      let request = {
-          location : latLng,
-          radius : 8047 ,
-          types: ["library"]
-      };
-      return new Promise((resolve,reject)=>{
-          service.nearbySearch(request,function(results,status){
-              if(status === google.maps.places.PlacesServiceStatus.OK)
-              {
-                  resolve(results);    
-              }else
-              {
-                  reject(status);
-              }
-
-          }); 
-      });
-
-  }
-  createMarker(place)
-  {
-      let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: place.geometry.location
-      });   
-  }  
-  addMap(lat,long){
-
-    let latLng = new google.maps.LatLng(lat, long);
-
-    let mapOptions = {
-    center: latLng,
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-    this.getRestaurants(latLng).then((results : Array<any>)=>{
-        this.places = results;
-        for(let i = 0 ;i < results.length ; i++)
-        {
-            this.createMarker(results[i]);
-        }
-    },(status)=>console.log(status));
-
-    this.addMarker();
-
 }
-}
+//   getRestaurants(latLng)
+//   {
+//       var service = new google.maps.places.PlacesService(this.map);
+//       let request = {
+//           location : latLng,
+//           radius : 8047 ,
+//           types: ["library"]
+//       };
+//       return new Promise((resolve,reject)=>{
+//           service.nearbySearch(request,function(results,status){
+//               if(status === google.maps.places.PlacesServiceStatus.OK)
+//               {
+//                   resolve(results);    
+//               }else
+//               {
+//                   reject(status);
+//               }
+
+//           }); 
+//       });
+
+//   }
+//   createMarker(place)
+//   {
+//       let marker = new google.maps.Marker({
+//       map: this.map,
+//       animation: google.maps.Animation.DROP,
+//       position: place.geometry.location
+//       });   
+//   }  
+//   addMap(lat,long){
+
+//     let latLng = new google.maps.LatLng(lat, long);
+
+//     let mapOptions = {
+//     center: latLng,
+//     zoom: 15,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//     }
+
+//     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+//     this.getRestaurants(latLng).then((results : Array<any>)=>{
+//         this.places = results;
+//         for(let i = 0 ;i < results.length ; i++)
+//         {
+//             this.createMarker(results[i]);
+//         }
+//     },(status)=>console.log(status));
+
+//     this.addMarker();
+
+// }
+// }
